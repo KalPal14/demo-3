3225fc972bae4f69814dd593958b926a2a3d9018
KalPal14 <reznkola@gmail.com>
2023-11-09 13:41:27 +0200

    49 Ответы клиенту

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import express from 'express'
 const port = 8000
 const app = express()
 
-app.all('/hello', (req, res, next) => {
-   console.log("All")
-   next()
-})
-
 app.get('/hello', (req, res) => {
+   res.cookie('token', 'devjoeinvevnreoineron', {
+      domain: '',
+      path: '/',
+      // нужно ли шифровать
+      secure: true,
+      // когда очистить ms
+      expires: 60000
+   })
    res.send('Привет!!!')
 })
 
