96ed8236b79c020cac84f6da815fe835540abf5d
KalPal14 <reznkola@gmail.com>
2023-11-09 13:10:08 +0200

    48 Маршрутизация

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import express from 'express'
 const port = 8000
 const app = express()
 
-app.get('/', (req, res) => {
-   res.send('Домашняя страница')
+app.all('/hello', (req, res, next) => {
+   console.log("All")
+   next()
 })
+
 app.get('/hello', (req, res) => {
    res.send('Привет!!!')
 })
