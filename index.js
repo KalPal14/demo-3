245af016bede9c476ea6ce60ead3007bf81cdc0d
KalPal14 <reznkola@gmail.com>
2023-11-09 16:07:43 +0200

    51 промежуточные обработчики

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ import { userRouter } from './users/users.js'
 const port = 8000
 const app = express()
 
+app.use('/hello', (res, req, next) => {}) // для всех роутов /hello
+
 app.get('/hello', (req, res) => {
-   res.send('Привет!!!')
+   throw new Error('Error!!!')
 })
 
 app.use('/users', userRouter)
+
+// Обработчик ошибок должен
+// находится под обработчиками роутов
+app.use((err, req, res, next) => {
+   res.status(401).send(err.message)
+})
 
 app.listen(port, () => {
    console.log(`Сервер запущен http://localhost:${port}`)
