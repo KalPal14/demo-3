060ba4090b18b5aa29b461db0ffc5867c36048a4
KalPal14 <reznkola@gmail.com>
2023-11-24 13:39:21 +0200

    82 Middleware для роутов

--- src/common/route.interface.ts
+++ src/common/route.interface.ts
@@ -1,7 +1,9 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import { IMiddleware } from './middleware.interface.js';
 
 export interface IRouterController {
 	path: string;
 	method: keyof Pick<Router, 'get' | 'post' | 'put' | 'delete' | 'patch'>;
 	func: (req: Request, res: Response, next: NextFunction) => void;
+	middlewares?: IMiddleware[];
 }
